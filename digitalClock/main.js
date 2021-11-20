c0f9035dc9d65adfa020fa71ff48f68c4f4985ae
rukyaislam <72370986+rukyaislam@users.noreply.github.com>
2021-11-20 22:56:43 +0600

    Add files via upload

---
+++ digitalClock/main.js
@@ -0,0 +1,35 @@
+  function  digital() {
+    let date=new Date();
+    let hours=date.getHours();
+    let minutes=date.getMinutes();
+    let seconds=date.getSeconds();
+let timeFormat="AM";
+
+if(hours==0)
+{
+    hours=12;
+
+}
+if(hours>12)
+{
+    hours=hours-12;
+    timeFormat="PM";
+}
+
+hours=hours<10? "0"+hours:hours;
+minutes=minutes<10? "0"+minutes:minutes;
+ seconds= seconds<10? "0"+ seconds: seconds;
+let finalTime=`${hours}:${minutes}:${seconds}`;
+
+
+
+
+
+document.getElementById("demo").innerText=finalTime;
+document.querySelector("small").innerText= timeFormat;
+
+
+
+
+}
+setInterval(digital,1000);
